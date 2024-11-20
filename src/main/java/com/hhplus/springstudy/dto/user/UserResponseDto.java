8119ece706f13e86fcd763e2d4d60ec9f1c0f26f
HanJeongSeol <hanjeong94@naver.com>
2024-11-20 16:13:06 +0900

    Feat: Dto 클래스 생성
    회원가입에 사용될 Dto 클래스 생성
    Related to : #14

---
+++ src/main/java/com/hhplus/springstudy/dto/user/UserResponseDto.java
@@ -0,0 +1,21 @@
+package com.hhplus.springstudy.dto.user;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class UserResponseDto {
+    private String userId;
+    private String userName;
+    private List<String> roles;
+
+    public UserResponseDto(String userId, String userName, List<String> roles) {
+        this.userId = userId;
+        this.userName = userName;
+        this.roles = roles;
+    }
+}

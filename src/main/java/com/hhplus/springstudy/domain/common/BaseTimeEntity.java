2b8939d28814b8c960e23d6ab3dba394632302c3
HanJeongSeol <hanjeong94@naver.com>
2024-11-20 11:40:10 +0900

    Feat: BaseTimeEntity 생성
    생성 및 수정 일자가 필요한 엔티티가 상속받아서 사용할 BaseTimeEntity.
    - @CreatedDate : 생성 일자 자동 추가
    - @LastModifiedDate : 데이터 수정 시 수정 시간으로 일자 업데이트
    Related to : #12

---
+++ src/main/java/com/hhplus/springstudy/domain/common/BaseTimeEntity.java
@@ -0,0 +1,24 @@
+package com.hhplus.springstudy.domain.common;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@MappedSuperclass
+@EntityListeners({AuditingEntityListener.class})
+public class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+}

ca4e8332c381986970c522a3c63060e981984f07
HanJeongSeol <hanjeong94@naver.com>
2024-11-27 10:56:52 +0900

    Test : 예외 처리 테스트
    - 예외 전달 테스트 진행
    - 글로벌 예외 핸들러는 컨트롤러 단계의 예외만 처리 -> 다른 방안 고려

--- src/main/java/com/hhplus/springstudy/config/security/JwtAuthenticationFilter.java
+++ src/main/java/com/hhplus/springstudy/config/security/JwtAuthenticationFilter.java
@@ -1,11 +1,13 @@
 package com.hhplus.springstudy.config.security;
 
 import com.hhplus.springstudy.config.jwt.JwtTokenProvider;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -21,6 +23,7 @@ import java.io.IOException;
  * OncePerRequestFilter는 SpringSecurity에서 제공하는 요청 당 한 번만 실행되는 필터이다.
  * 해당 클래스를 상속받아서 구현하자.
  */
+@Slf4j
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtTokenProvider jwtTokenProvider;
@@ -28,28 +31,40 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try{
+            // 0. SecurityContext에 인증 정보가 이미 있다면 JWT 검증 스킵
+            if (SecurityContextHolder.getContext().getAuthentication() != null) {
+                filterChain.doFilter(request, response);
+                return;
+            }
 
-        // 0. SecurityContext에 인증 정보가 이미 있다면 JWT 검증 스킵
-        if (SecurityContextHolder.getContext().getAuthentication() != null) {
-            filterChain.doFilter(request, response);
+            // 1. Authorization 헤더에서 JWT 추출
+            String jwt = extractJwtFromRequest(request);
+
+            // 2. JWT 검증 및 사용자 정보 설정
+            if (StringUtils.hasText(jwt) && jwtTokenProvider.validateToken(jwt)) {
+                String username = jwtTokenProvider.extractUserId(jwt);
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                // SecurityContext에 인증 정보 저장
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }catch(ExpiredJwtException e){
+            log.warn("토큰이 만료되었습니다 : {} ", e.getMessage());
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("Token expired");
             return;
         }
+        catch (Exception e){
+            log.error("사용자 인증에 실패했습니다.", e);
 
-        // 1. Authorization 헤더에서 JWT 추출
-        String jwt = extractJwtFromRequest(request);
-
-        // 2. JWT 검증 및 사용자 정보 설정
-        if (StringUtils.hasText(jwt) && jwtTokenProvider.validateToken(jwt)) {
-            String username = jwtTokenProvider.extractUserId(jwt);
-            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-
-            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-
-            // SecurityContext에 인증 정보 저장
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("Unauthorized: " + e.getMessage());
+            return;
         }
-
         // 3. 다음 필터로 요청 전달
         filterChain.doFilter(request, response);
     }
@@ -59,6 +74,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
             return bearerToken.substring(7);
         }
+        log.warn("유효하지 않은 헤더 인증 요청 : {}", bearerToken);
         return null;
     }
 }

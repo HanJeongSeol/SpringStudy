7fd2a6cb6877ddf6247f9c7236809144d648c2b1
HanJeongSeol <hanjeong94@naver.com>
2024-11-27 13:35:31 +0900

    Feat : CustomAuthenticationEntryPoint, CustomAccessDeniedHandler 구현
    - 인증 실패 시 호출되는 CustomAuthenticationEntryPoint
    - 인가 실패 시 호출되는 CustomAccessDeniedHandler
    - GlobalExceptionHandler를 사용하여 일관된 예외 데이터를 전달하기 위해 HandlerExceptionResolver를 사용하여 GlobalExceptionHandler로 예외 위임

---
+++ src/main/java/com/hhplus/springstudy/config/security/CustomAccessDeniedHandler.java
@@ -0,0 +1,34 @@
+package com.hhplus.springstudy.config.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import java.io.IOException;
+
+/**
+ * Spring Security 필터 체인에서 인증이 완료되었지만 리소스 접근 권한이 없을 때 호출된다.
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final HandlerExceptionResolver handlerExceptionResolver;
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+        // 로그 기록
+        log.error("Access denied: {}", accessDeniedException.getMessage());
+
+        // HandlerExceptionResolver를 통해 GlobalExceptionHandler로 예외 위임
+        handlerExceptionResolver.resolveException(request, response, null, accessDeniedException);
+    }
+}

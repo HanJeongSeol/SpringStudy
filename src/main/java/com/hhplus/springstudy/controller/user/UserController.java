dec38f8fe62d53c0182e3acd3170279cc1fd6f99
HanJeongSeol <hanjeong94@naver.com>
2024-12-04 13:11:26 +0900

    Feat : 아이디, 패스워드 유효성 검사 적용
    - 회원 가입 진행 시 클라이언트에게 전달받은 아이디, 비밀번호 데이터 유효성 검사 진행
    Reate to : #46

--- src/main/java/com/hhplus/springstudy/controller/user/UserController.java
+++ src/main/java/com/hhplus/springstudy/controller/user/UserController.java
@@ -6,6 +6,7 @@ import com.hhplus.springstudy.dto.user.UserRequestDto;
 import com.hhplus.springstudy.dto.user.UserResponseDto;
 import com.hhplus.springstudy.dto.user.UserSaveRequestDto;
 import com.hhplus.springstudy.service.user.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,11 +21,11 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/join")
-    public ResponseEntity<ApiResponse<UserResponseDto>> registerUser(@RequestBody UserSaveRequestDto requestDto){
+    public ResponseEntity<ApiResponse<UserResponseDto>> registerUser(@Valid @RequestBody UserSaveRequestDto requestDto){
         UserResponseDto responseDto = userService.registerUser(requestDto);
         return ResponseEntity.ok(ApiResponse.of(SuccessCode.USER_CREATE_SUCCESS, responseDto));
     }
-
+d
     @PostMapping
     public ResponseEntity<ApiResponse<UserResponseDto>> loginUser(@RequestBody UserRequestDto requestDto){
         UserResponseDto responseDto = userService.loginUser(requestDto);
--- src/main/java/com/hhplus/springstudy/dto/user/UserSaveRequestDto.java
+++ src/main/java/com/hhplus/springstudy/dto/user/UserSaveRequestDto.java
@@ -1,5 +1,8 @@
 package com.hhplus.springstudy.dto.user;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -8,7 +11,15 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 public class UserSaveRequestDto {
+    @NotBlank(message = "아이디는 필수 입력 값입니다.")
+    @Size(min = 4, max =10, message = "아이디는 4자 이상, 10자 이하여야 합니다.")
+    @Pattern(regexp ="^[a-z0-9]+$", message = "아이디는 소문자와 숫자만 사용 가능합니다.")
     private String userId;
+
+    @NotBlank(message="비밀번호는 필수 입력 값입니다.")
+    @Size(min = 8, max = 15, message="비밀번호는 8자 이상, 15자 이하여야 합니다.")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,15}$",
+            message = "비밀번호는 대문자, 소문자, 숫자, 특수문자가 포함되야 합니다.")
     private String userPassword;
     private String userName;
     private List<String> roleIds;  // ROLE_USER, ROLE_ADMIN
